90ccbead708825cb7840df66d481bc110f4ef087
Emiliano Aguilar <134093364+GustavoContreras511@users.noreply.github.com>
2024-04-24 09:59:31 -0600

    si pasamos o ne

--- src/app/datalogs/datalogs.component.ts
+++ src/app/datalogs/datalogs.component.ts
@@ -1,7 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import {DataService} from './data.service'
 import {Data} from '../interfaces/data'
+import { interval, Subscription } from 'rxjs';
+
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-datalogs',
@@ -10,20 +13,37 @@ import {Data} from '../interfaces/data'
   templateUrl: './datalogs.component.html',
   styleUrl: './datalogs.component.css'
 })
-export class DatalogsComponent implements OnInit {
+export class DatalogsComponent implements OnInit, OnDestroy {
   Data: Data[] = [];
-
+  private pollingSubscription: Subscription | null = null;
   constructor(private dataService: DataService) { }
   ngOnInit(): void {
-    this.dataService.getLogs().subscribe(
-      (data) => {
-        console.log('Datos recibidos:', data); // Verifica los datos recibidos en la consola
-        this.Data = data;
-      },
-      (error) => {
-        console.error('Error al obtener los datos:', error);
-      }
+    this.startPolling(
     );
+  }
+  ngOnDestroy(): void {
+    this.stopPolling();
+  }
+  startPolling(): void {
+    this.pollingSubscription = interval(5000)
+      .pipe(
+        switchMap(() => this.dataService.getLogs())
+      )
+      .subscribe(
+        (data: Data[]) => {
+          console.log('datos actualizados')
+          this.Data = data;
+        },
+        (error: any) => {
+          console.error('Error al obtener los datos:', error);
+        }
+      );
+  }
+
+  stopPolling(): void {
+    if (this.pollingSubscription) {
+      this.pollingSubscription.unsubscribe();
+    }
   }
 }
  
